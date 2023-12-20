b493ed99ef1ce0c4c0d7d687bcbd22be0914c4af
Alan Solima <alancontato03@gmail.com>
2023-12-20 14:49:42 -0300

    First commit - Working API RESTful

---
+++ src/main/java/com/restful/springboot/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.restful.springboot.repositories;
+
+
+import com.restful.springboot.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+}
